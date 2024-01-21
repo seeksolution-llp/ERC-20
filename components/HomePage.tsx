747ffcd978842a920abe70f43dc2377cf3888a5a
Ham Mim <ehisab.india@gmail.com>
2024-01-21 10:44:19 +0530

    test approve

---
+++ components/HomePage.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { HenceforthContracts } from "@/utils/SeekSolutionEthereum"
+import { Button, Input } from "antd"
+import React, { Fragment } from "react"
+
+const HomePage = () => {
+    const [token, setToken] = React.useState(0)
+
+    const handleClick = async () => {
+        const eth = new HenceforthContracts()
+        const instance = await eth.contractInstance()
+        await eth.transfer(instance, token)
+    }
+
+    return <Fragment>
+        <Input onChange={(e) => setToken(Number(e.target.value))} />
+        <Button onClick={handleClick}>Approve</Button>
+    </Fragment>
+
+}
+
+export default HomePage
